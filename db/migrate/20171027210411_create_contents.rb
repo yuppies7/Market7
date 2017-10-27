3dd08a83f5491ce0a9e7a95374b42234ebea2948
yuppies7 <andrea.cappicciola@gmail.com>
2017-10-27 23:07:29 +0200

    creato scaffold contenuti

---
+++ db/migrate/20171027210411_create_contents.rb
@@ -0,0 +1,11 @@
+class CreateContents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
